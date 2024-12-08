bf8219e0ac65d8b64a28985c8ce392ca31d25915
Ahmed El-Kholy <a.adel27111@gmail.com>
2024-12-08 17:40:04 +0200

    Q2

--- Assignement/Program.cs
+++ Assignement/Program.cs
@@ -10,6 +10,18 @@
             //Console.WriteLine(Num);
             #endregion
 
+            #region Q2 Write C# program that converts a string to an integer, but the string contains non-numeric characters.And mention what will happen
+            //int Num;
+            //string Name = "Ahmed23";
+            //int.TryParse(Name, out Num);
+            //Console.WriteLine(Num);
+
+            /**
+             * Result:
+             * Num will be 0 because TryParse Can't convert the string
+             * and TryParse give the variable the default Value of it int => 0
+             */
+            #endregion
 
         }
     }
