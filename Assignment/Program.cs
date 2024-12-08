b8dd0581decb659e7fbee3b00542e138cb2171f7
midoa <midoa@KHOLY>
2024-12-08 17:15:31 +0200

    Add project files.

---
+++ Assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using static System.Collections.Specialized.BitVector32;
+using System.Drawing;
+
+namespace Assignment
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+        }
+
+
+    }
+}
---
+++ Demo/main.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Demo
+{
+    internal class Program
+    {
+
+    }
+
+}
