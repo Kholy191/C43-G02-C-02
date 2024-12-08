a9b4ac7e7ff97d651cbd9d04cd2b1d5c0adc1b0e
Ahmed El-Kholy <a.adel27111@gmail.com>
2024-12-08 17:41:30 +0200

    Q3

--- Assignement/Program.cs
+++ Assignement/Program.cs
@@ -23,6 +23,18 @@
              */
             #endregion
 
+
+            #region Q3 Write C# program that Perform a simple arithmetic operation with floating - point numbers And mention what will happen
+            //float Num1 = 4.5F;
+            //float Num2 = 5.3F;
+            //Console.WriteLine($"Result = {Num1 + Num2}");
+
+            /**
+             * Result:
+             * Just added numbers
+             */
+            #endregion
+
         }
     }
 }
