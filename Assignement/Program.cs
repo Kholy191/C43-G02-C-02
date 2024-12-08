1a6e20be0d845f4854f51ec0f0bdba5258e8099f
Ahmed El-Kholy <a.adel27111@gmail.com>
2024-12-08 17:38:02 +0200

    Q1

---
+++ Assignement/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Assignement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1 Write a program that allows the user to enter a number then print it
+            //int Num;
+            //int.TryParse(Console.ReadLine(), out Num);
+            //Console.WriteLine(Num);
+            #endregion
+
+
+        }
+    }
+}
